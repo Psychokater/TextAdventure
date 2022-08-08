3d4707c6bfa6a664f267dc503a2059d42053c18d
Psychokater <psychokater@protonmail.com>
2022-08-08 15:52:16 +0200

    Added LevelUp

--- TextAdventure/Stats.py
+++ TextAdventure/Stats.py
@@ -28,3 +28,16 @@ def EditStats(playerStats, playerStatPoints):
             case "4": break
             case _: print("\nCouldn't understand you?!")
     return playerStats, playerStatPoints
+
+def LevelUp(playerStats, playerStatPoints):
+    # Playerstats = 0 Level, 1 MAX HP, 2 HP, 3 ATK, 4 DEF, 5 EXP
+
+    if playerStats[5] == playerStats[0] * round((2**(playerStats[0]*0.6))):
+        playerStats[0] += 1
+        if playerStats[0] % 5 == 0:
+            playerStatPoints += 4
+        else:
+            playerStatPoints += 2
+        print("\nYay, Level Up!")
+        
+    return playerStats, playerStatPoints
