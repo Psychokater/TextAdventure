cf13715f5a840abf4fccf8645180d75f11df2170
Psychokater <psychokater@protonmail.com>
2022-08-09 12:43:58 +0200

    ---

--- TextAdventure/TestbereichNils.py
+++ TextAdventure/TestbereichNils.py
@@ -1,12 +1,8 @@
- 
-playerName = ".aasd"
-playerInventoryMoney = 2.5512312
 
+for x in range(1,21):
+    y = round(100*x**(1.5),2)
+    print(f"\t{y} ",end='\t')
+    if x % 5 == 0:
+        print("lvl = ",x)
 
-print(f"""
-{playerName}
-\nGold: {round(playerInventoryMoney,2)}
-In
-
-(1) Return\n
-""")            
+    # y = 100 * x ^ 1.5
