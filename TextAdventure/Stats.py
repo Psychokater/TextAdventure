d258a67286de3497fc7de0f1069785c0a2504cd6
Psychokater <psychokater@protonmail.com>
2022-08-09 18:48:01 +0200

    fixed Hotfix

--- TextAdventure/Stats.py
+++ TextAdventure/Stats.py
@@ -11,7 +11,7 @@ def StatMenu(playerStats, playerStatPoints, playerName):
         else: 
             userInput = input(f"\n(1) Edit Stats ({playerStatPoints}P)\t(2) Return\n")
             if userInput == "1":
-                playerStats, playerStatpoints = EditStats(playerStats, playerStatPoints, playerName)
+                playerStats, playerStatPoints = EditStats(playerStats, playerStatPoints, playerName)
             elif userInput == "2":
                 return
             else: print("\nCouldn't understand you?!")
@@ -20,17 +20,26 @@ def StatMenu(playerStats, playerStatPoints, playerName):
 def EditStats(playerStats, playerStatPoints, playerName):
      # Playerstats = 0 Level, 1 MAX HP, 2 HP, 3 ATK, 4 DEF, 5 EXP
     while True:
+        if playerStatPoints == 0:
+            break
         nextLevelExp = playerStats[0] * round((100*(playerStats[0]**1.5)),2)
         print(f"\n{playerName}\n\nPoints: {playerStatPoints}\t\t\tEXP: {playerStats[5]}/{nextLevelExp}\n\nHP: {playerStats[2]}/{playerStats[1]}\nATK: {playerStats[3]}\nDEF: {playerStats[4]}")
         userInput = input("\n(1) HP +10\t (2) Atk + 1\t (3) Def + 1\t (4) Return\n")
-        match userInput:
-            case "1": playerStats[1] += 10; playerStatPoints -= 1
-            case "2": playerStats[3] += 1; playerStatPoints -= 1
-            case "3": playerStats[4] += 1; playerStatPoints -= 1
-            case "4": break
-            case _: print("\nCouldn't understand you?!")
-        if playerStatPoints == 0:
+        if userInput == "1":
+            playerStats[1] += 10
+            playerStatPoints -= 1
+        elif userInput == "2":
+            playerStats[3] += 10
+            playerStatPoints -= 1
+        elif userInput == "3":
+            playerStats[4] += 10 
+            playerStatPoints -= 1
+        elif userInput == "4":
             break
+        else:
+            print("\nCouldn't understand you?!")        
+           
+
     return playerStats, playerStatPoints
 
 def LevelUp(playerStats, playerStatPoints, playerName):
