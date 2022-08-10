63111416194abae47d1de793afb905784c08534c
Psychokater <psychokater@protonmail.com>
2022-08-10 19:46:12 +0200

    added Inventory

--- TextAdventure/TestbereichNils.py
+++ TextAdventure/TestbereichNils.py
@@ -1,79 +1,8 @@
 import os
 
-######## call Itemlist of Player (Index 8 != 0)
-def call_P_list(all_items):
-    print ('Your Items : ')
-    print (' Item.NR      Item    Attack  Defence  Heal   Quantity ')
-    z = 1
-    for i in range (0,len(all_items)):
-        k = i + 1001
-        inputListeP = []
-        if all_items[k][9] > 0:
-            all_items[k][1] = z
-            z += 1            
-            inputListeP.append(i)
-            print ('\u2009 ',all_items[k][1],end='  :  ')
-            for j in range (2,len(all_items[k])-4):
-                print (all_items[k][j],end='\t')
-            print (all_items[k][9])
-
-    return inputListeP, all_items
-
-
-def call_M_list(all_items):
-    print ('Merchant Items : ')
-    print ('  Item.NR      Item    Attack  Defence   Heal   Value   Price  Quantity ')
-    z = 1
-    for i in range (0,len(all_items)):
-        i += 1001
-        inputListeM = []
-        if all_items[i][8] > 0:
-            all_items[i][0] = z
-            z += 1            
-            inputListeM.append(i)
-            print ('\u2009 ',all_items[i][0],end='  :  ')
-            for j in range (1,len(all_items[i])-1):
-                if j == 1:
-                    continue
-                print (all_items[i][j],end='\t')
-            print ()
-
-    return inputListeM, all_items
-
-def buy_anItem(all_items):
-    os.system('cls')
-    inputListeM, all_items = call_M_list(all_items)
-    inputListeP, all_items = call_P_list(all_items)
-    Item_number = int (input ('Pick an Item number to buy it : \n'))
-    for i in range (0,len(all_items)):
-        i +=1001       
-        if Item_number == all_items[i][0]:
-            all_items[i][8] -= 1
-            all_items[i][9] += 1
-            if all_items[i][8] == 0:
-                all_items[i][0] = 0
-
-    return all_items
-
-def sell_anItem(all_items):
-    os.system('cls')
-    inputListeM, all_items = call_M_list(all_items)
-    inputListeP, all_items = call_P_list(all_items)    
-    Item_number = int (input ('Pick an Item number to sell it : \n'))
-    for i in range (0,len(all_items)):
-        i +=1001
-        if Item_number == all_items[i][1]:
-            all_items[i][8] += 1
-            all_items[i][9] -= 1
-            if all_items[i][9] == 0:
-                all_items[i][1] = 0
-                               
-    return all_items
-
-
-
-def shop_menu():
-        all_items = {
+#ENTRYPOINT (change later?!) (Choose where to go -> Merchant or Inventory)
+def ShopMenu():
+        itemsDict = {
         #                             atk def  hp val  pr  qnt  qnt  
         #                                                   M    P
         1001:[(0),(0),"Apple     ==>",(0),(0),(4),(1),(99),(1),(1)],
@@ -86,36 +15,41 @@ def shop_menu():
         1008:[(0),(0),"ItemName7 ==>",(9),(8),(0),(6),(99),(99),(0)],
         1009:[(0),(0),"ItemName8 ==>",(9),(8),(1),(6),(99),(98),(0)],
         1010:[(0),(0),"ItemName9 ==>",(9),(8),(1),(6),(99),(1),(0)]}
-            
+        
         while True:   
             userInput = input("\nWhere do you want to go?\n(1) Merchant\t(2) Inventory\t(3) Exit\n")      
             match userInput:
-                case "1": all_items, inputListeP, inputListeM = merchant(all_items, inputListeP, inputListeM)
-
-                case "2": inputListeP, all_items = inventory(inputListeP, all_items)
-
+                case "1": itemsDict = MerchantShop(itemsDict)                                                           
+                case "2": itemsDict = InventoryMenu(itemsDict)                                                          
                 case "3": break
 
                 case _: print("\nCouldn't understand you?!")
 
-def merchant(all_items, ):
-    os.system('cls')
-    while True:
-        inputListeM, all_items = call_M_list(all_items)
-        inputListeP, all_items = call_P_list(all_items)
 
-        userInput = input("\nWhat whould you like ?\n(1) Buy an Item\t\t(2) Sell an Item\t(3) Leave Merchant\n")      
+# Merchant                                (Print Merchant+PlayerInventory - Choose if Buy or Sell)
+def MerchantShop(itemsDict):
+    os.system('cls')
+    playerItemIDs = []
+    merchantItemIDs = []
+    while True:
+        itemsDict, merchantItemIDs = GetInventoryMerchant(itemsDict, merchantItemIDs)
+        itemsDict, playerItemIDs = GetInventoryPlayer(itemsDict, playerItemIDs)
+
+        userInput = input("\nWhat whould you like ?\n(1) Buy\t\t(2) Sell\t(3) Leave Merchant\n")      
         match userInput:
-            case "1": all_items = buy_anItem(all_items)
-            case "2": all_items = sell_anItem(all_items)
+            case "1": itemsDict, playerItemIDs, merchantItemIDs = MerchantItemBuy(itemsDict, playerItemIDs, merchantItemIDs)
+            case "2": itemsDict, playerItemIDs, merchantItemIDs = MerchantItemSell(itemsDict, playerItemIDs, merchantItemIDs)
             case "3": break
             case _: print("\nCouldn't understand you?!")
     
-    return all_items, inputListeP, inputListeM
+    return itemsDict
 
-def inventory(inputListeP, all_items):
+# PlayerInventory                      (Choose "Equip", "Remove"---- function for later)
+def InventoryMenu(itemsDict):
+    os.system('cls')
+    playerItemIDs = []
     while True:
-        inputListeP, all_items = call_P_list(all_items)
+        itemsDict, playerItemIDs = GetInventoryPlayer(itemsDict , playerItemIDs)
         userInput = input("\n(1) Equip\t\t(2) Remove from Inventory\t(3) Leave Inventory\n")
         if userInput == "1":
             pass
@@ -123,5 +57,81 @@ def inventory(inputListeP, all_items):
             pass
         elif userInput == "3": break
         else: print("\nCouldn't understand you?!")
-    return inputListeP, all_items
-shop_menu()
+    
+    return itemsDict
+
+# print PlayerInventory + getting ID's
+def GetInventoryPlayer(itemsDict, playerItemIDs):
+    print ('Your Items : ')
+    print (' Item.NR      Item    Attack  Defence  Heal   Quantity ')
+    z = 1
+    for i in range (0,len(itemsDict)):
+        k = i + 1001
+        playerItemIDs = []
+        if itemsDict[k][9] > 0:
+            itemsDict[k][1] = z
+            z += 1            
+            playerItemIDs.append(i)
+            print ('\u2009 ',itemsDict[k][1],end='  :  ')
+            for j in range (2,len(itemsDict[k])-4):
+                print (itemsDict[k][j],end='\t')
+            print (itemsDict[k][9])
+
+    return itemsDict, playerItemIDs
+
+# print MerchantInventory + getting iD's
+def GetInventoryMerchant(itemsDict, merchantItemIDs):
+    print ('Merchant Items : ')
+    print ('  Item.NR      Item    Attack  Defence   Heal   Value   Price  Quantity ')
+    z = 1
+    for i in range (0,len(itemsDict)):
+        i += 1001
+        if itemsDict[i][8] > 0:
+            itemsDict[i][0] = z
+            z += 1            
+            merchantItemIDs.append(i)
+            print ('\u2009 ',itemsDict[i][0],end='  :  ')
+            for j in range (1,len(itemsDict[i])-1):
+                if j == 1:
+                    continue
+                print (itemsDict[i][j],end='\t')
+            print ()
+
+    return itemsDict, merchantItemIDs
+
+
+def MerchantItemBuy(itemsDict, playerItemIDs, merchantItemIDs):
+    os.system('cls')
+    itemsDict, merchantItemIDs = GetInventoryMerchant(itemsDict, merchantItemIDs)
+    itemsDict, playerItemIDs = GetInventoryPlayer(itemsDict, playerItemIDs)
+    userInputItemNumber = int (input ('Pick an Item number to buy it : \n'))
+    for i in range (0,len(itemsDict)):
+        i +=1001       
+        if userInputItemNumber == itemsDict[i][0]:
+            itemsDict[i][8] -= 1
+            itemsDict[i][9] += 1
+            if itemsDict[i][8] == 0:
+                itemsDict[i][0] = 0
+
+    return itemsDict, playerItemIDs, merchantItemIDs
+
+
+def MerchantItemSell(itemsDict, playerItemIDs, merchantItemIDs):
+    os.system('cls')
+    itemsDict, merchantItemIDs = GetInventoryMerchant(itemsDict, merchantItemIDs)
+    itemsDict, playerItemIDs = GetInventoryPlayer(itemsDict, playerItemIDs)    
+    userInputItemNumber = int (input ('Pick an Item number to sell it : \n'))
+    for i in range (0,len(itemsDict)):
+        i +=1001
+        if userInputItemNumber == itemsDict[i][1]:
+            itemsDict[i][8] += 1
+            itemsDict[i][9] -= 1
+            if itemsDict[i][9] == 0:
+                itemsDict[i][1] = 0
+                               
+    return itemsDict, playerItemIDs, merchantItemIDs
+
+
+
+
+ShopMenu()
