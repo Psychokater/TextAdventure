b29cf89c5ea0d45916d90ffc878bf987b54170c2
Psychokater <psychokater@protonmail.com>
2022-08-10 12:12:39 +0200

    fix for Samo...

--- TextAdventure/TestbereichNils.py
+++ TextAdventure/TestbereichNils.py
@@ -1,19 +1,22 @@
+
+
+######## call Itemlist of Player (Index 8 != 0)
 def call_P_list(all_items):
     print ('Your Items : ')
     print (' Item.NR      Item    Attack  Defence  Heal   Quantity ')
     z = 1
     for i in range (0,len(all_items)):
-        i +=1001
-        if all_items[i][8] > 0:
-            all_items[i][0]=z
-            z+=1
-            inputliste = []
-            inputliste.append(i)
-            print ('\u2009 ',all_items[i][0],end='  :  ')
-            for j in range (1,len(all_items[i])-4):
-                print (all_items[i][j],end='\t')
-            print (all_items[i][8])
-    return all_items
+        k = i + 1001
+        if all_items[k][8] > 0:
+            all_items[k][0] = z
+            z += 1
+            inputlisteP = []
+            inputlisteP.append(i)
+            print ('\u2009 ',all_items[k][0],end='  :  ')
+            for j in range (1,len(all_items[k])-4):
+                print (all_items[k][j],end='\t')
+            print (all_items[k][8])
+    return inputlisteP, all_items
 
 
 def call_M_list(all_items):
@@ -21,37 +24,40 @@ def call_M_list(all_items):
     print ('  Item.NR      Item    Attack  Defence   Heal   Value   Price  Quantity ')
     z = 1
     for i in range (0,len(all_items)):
-        i +=1001
-        if all_items[i][7] > 0:
-            all_items[i][0]=z
-            z+=1
-            inputliste = []
-            inputliste.append(i)
-            print ('\u2009 ',all_items[i][0],end='  :  ')
-            for j in range (1,len(all_items[i])-1):
-                print (all_items[i][j],end='\t')
+        k = i + 1001
+        if all_items[k][7] > 0:
+            all_items[k][0] = z
+            z += 1
+            inputlisteM = []
+            inputlisteM.append(i)
+            print ('\u2009 ',all_items[k][0],end='  :  ')
+            for j in range (1,len(all_items[k])-1):
+                print (all_items[k][j],end='\t')
             print ()
-    return all_items
+    return inputlisteM, all_items
 
 def buy_anItem(all_items):
-    all_items = call_M_list(all_items)
+    inputlisteM, all_items = call_M_list(all_items)
     Item_number = int (input ('Pick an Item number to buy it : \n'))
-    for i in range (0,len(inputliste)):
+    for i in range (0,len(inputlisteM)):
         i +=1001
         Item_number+=1000
-        if Item_number == all_items[i][0]:
-            all_items[Item_number][7]-=1
-            all_items[Item_number][8]+=1
-            all_items = call_P_list(all_items)
+        if Item_number in all_items:
+            all_items[Item_number][7] -= 1
+            all_items[Item_number][8] += 1
+            inputlisteM, all_items = call_P_list(all_items)
     return all_items
 
 def sell_anItem(all_items):
-    all_items = call_P_list(all_items)
+    inputlisteP, all_items = call_P_list(all_items)
     Item_number = int (input ('Pick an Item number to sell it : \n'))
-    if Item_number in all_items:
-        all_items[Item_number][6]+=1
-        all_items[Item_number][7]-=1
-        all_items = call_P_list(all_items)
+    for i in range (0,len(inputlisteP)):
+        i +=1001
+        Item_number+=1000
+        if Item_number in all_items:
+            all_items[Item_number][7] += 1
+            all_items[Item_number][8] -= 1
+            inputlisteP, all_items = call_P_list(all_items)
     return all_items
 
 def shop_menu():
@@ -69,17 +75,17 @@ def shop_menu():
         1009:[(0),"ItemName8 ==>",(9),(8),(1),(6),(99),(99),(1)],
         1010:[(0),"ItemName9 ==>",(9),(8),(1),(6),(99),(99),(1)]}
 
+        while True:
+            userInput = input("\nWhat whould you like ?\n(1) Buy an Item\t\t(2) Sell an Item\t(3) Inventory\t  (4) Exit to main menu\n")
+            match userInput:
+                case "1": all_items = buy_anItem(all_items)
 
-        userInput = input("\nWhat whould you like ?\n(1) Buy an Item\t\t(2) Sell an Item\t(3) Inventory\t  (4) Exit to main menu\n")
-        match userInput:
-            case "1": all_items = buy_anItem(all_items)
+                case "2": all_items = sell_anItem(all_items)
 
-            case "2": all_items = sell_anItem(all_items)
+                case "3": all_items = call_P_list(all_items)
 
-            case "3": all_items = call_P_list(all_items)
+                case "4": break
 
-            # case "4":
-            case _: print("\nCouldn't understand you?!")
-        return all_items
+                case _: print("\nCouldn't understand you?!")
 
 shop_menu()
