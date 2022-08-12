53ef644abfe02f965ce9b00a08d1b3dbdb91e721
Psychokater <psychokater@protonmail.com>
2022-08-12 08:26:31 +0200

    Style Change

--- TextAdventure/Main.py
+++ TextAdventure/Main.py
@@ -114,16 +114,16 @@ def IngameMenu(playerName, startLocation, location):
     #      0   1        2            3     4       5      6         7        8       9       10     11
     #                  Name         atk    def     hp      val      qnt      qnt      ID    ID_ON  use/eq                           Quantity Merchant resets every lvlUp (qnt Max / 2 - qnt P)
     #                                                                Max       P                                                     qnt Max = Max qnt in Game
-    1001:[(0),(0),"Apple\t",        (0),   (0),    (2),    (1),     (20),    (5),    (7),    (7),   (0)],                          # ID (row 9 and 10)
+    1001:[(0),(0),"Apple\t",        (0),   (0),    (2),    (1),     (20),    (2),    (7),    (7),   (0)],                          # ID (row 9 and 10)
     1002:[(0),(0),"Fish\t",         (0),   (0),    (5),    (3),     (20),    (0),    (7),    (7),   (0)],                          # 1, 2, 3 = Merchant Inv lvl 5, 10, 20+
     1003:[(0),(0),"Cabbage\t",      (0),   (0),    (4),    (2),     (10),    (0),    (7),    (7),   (0)],                          # 4, 5, 6 = Wizard Inv lvl 5, 10, 20+   
-    1004:[(0),(0),"Rusty Sword",    (5),   (0),    (0),    (5),     (2),     (0),    (1),    (1),   (1)],                         # 7, 8, 9 = Loot Inv lvl 5, 10, 20+                          
+    1004:[(0),(0),"Rusty Sword",    (5),   (0),    (0),    (5),     (2),     (0),    (1),    (1),   (1)],                          # 7, 8, 9 = Loot Inv lvl 5, 10, 20+                          
     1005:[(0),(0),"Shield\t",       (0),   (5),    (0),    (8),     (2),     (0),    (1),    (1),   (2)],                          # 
     1006:[(0),(0),"10 HP Potion",   (0),   (0),    (10),   (5),     (10),    (0),    (4),    (4),   (0)],                          # For Index 11: Use = 0, Weapon = 1, Secondary = 2, Head = 3, Breast = 4, Feet = 5, Hands = 6, Special = 7 -- if equipped: +10
     1007:[(0),(0),"20 HP Potion",   (0),   (0),    (20),   (10),    (10),    (0),    (5),    (0),   (0)],                          # 
     1008:[(0),(0),"30 HP Potion",   (0),   (0),    (30),   (15),    (10),    (0),    (6),    (0),   (0)],                          # IF lvl "x" Then itemsDict(10) = itemsDict(9)
-    1009:[(0),(0),"Bandage",        (0),   (0),    (5),    (3),     (5),     (0),    (4),    (4),   (0)],                        
-    1010:[(0),(0),"Bullwark",       (0),   (15),   (0),    (17),    (1),     (0),    (9),    (0),   (2)],
+    1009:[(0),(0),"Bandage\t",      (0),   (0),    (5),    (3),     (5),     (2),    (4),    (4),   (0)],                        
+    1010:[(0),(0),"Bullwark",       (0),   (15),   (0),    (17),    (1),     (0),    (9),    (0),   (2)],                         
     1011:[(0),(0),"Steel Plates",   (0),   (12),   (0),    (20),    (1),     (0),    (3),    (0),   (3)],                        
     1012:[(0),(0),"Steel Gloves",   (0),   (5),    (0),    (7),     (1),     (0),    (3),    (0),   (6)],                        
     1013:[(0),(0),"Steel Shoes",    (0),   (3),    (0),    (5),     (1),     (0),    (3),    (0),   (5)],                        # IF itemsDict(10)  = Value = Item is in game
@@ -131,21 +131,21 @@ def IngameMenu(playerName, startLocation, location):
     1015:[(0),(0),"Steel Shield",   (0),   (10),   (0),    (15),    (1),     (0),    (3),    (0),   (2)],                         # IF itemsDict(10)  = 0 = Item is NOT in game!  
     1016:[(0),(0),"Steel Axe",      (10),  (0),    (0),    (15),    (1),     (0),    (3),    (0),   (1)],                        
     1017:[(0),(0),"Blueberries",    (0),   (0),    (4),    (2),     (10),    (0),    (7),    (7),   (0)],                        
-    1018:[(0),(0),"Dark Dagger",    (15),  (0),    (0),    (30),    (1),     (0),    (8),    (0),   (1)], 
-    1019:[(0),(0),"Bone Knife",     (0),   (0),    (0),    (1),     (1),     (0),    (8),    (0),   (1)],                        
-    1020:[(0),(0),"Trousers\t",     (0),   (2),    (0),    (4),     (4),     (1),    (1),    (1),   (4)],                        
+    1018:[(0),(0),"Dark Dagger",    (12),  (0),    (0),    (18),    (1),     (0),    (8),    (0),   (1)], 
+    1019:[(0),(0),"Bone Knife",     (10),  (0),    (0),    (16),    (1),     (0),    (8),    (0),   (1)],                        
+    1020:[(0),(0),"Trousers",       (0),   (2),    (0),    (4),     (4),     (1),    (1),    (1),   (14)],                        
     1021:[(0),(0),"Butcher Knife",  (3),   (0),    (0),    (6),     (3),     (0),    (7),    (7),   (1)],                        
-    1022:[(0),(0),"Broken Staff",   (2),   (0),    (0),    (1),     (3),     (1),    (7),    (7),   (1)],                       
-    1023:[(0),(0),"Kings Sword",    (25),  (0),    (0),    (50),    (1),     (0),    (9),    (0),   (1)],                        
-    1024:[(0),(0),"Chicken\t",      (0),   (0),    (8),    (10),    (5),     (0),    (8),    (0),   (0)],                        
-    1025:[(0),(0),"Exp Scroll",     (0),   (0),    (0),    (70),    (1),     (0),    (6),    (0),   (0)],                        
-    1026:[(0),(0),"Letttuce",       (0),   (0),    (2),    (1),     (10),    (1),    (7),    (0),   (0)],                        
-    1027:[(0),(0),"Muddy Gloves",   (0),   (1),    (0),    (1),     (5),     (1),    (7),    (0),   (6)],                         
+    1022:[(0),(0),"Broken Staff",   (2),   (0),    (0),    (1),     (3),     (1),    (7),    (7),   (11)],                       
+    1023:[(0),(0),"Kings Sword",    (25),  (0),    (0),    (50),    (1),     (0),    (9),    (0),   (1)],                          #ItemKeys: 1234 
+    1024:[(0),(0),"Chicken\t",      (0),   (0),    (8),    (10),    (5),     (0),    (8),    (0),   (0)],                          #          1 = lvl range (0-10, 10-20, 20+)  
+    1025:[(0),(0),"Exp Scroll",     (0),   (0),    (0),    (70),    (1),     (0),    (6),    (0),   (0)],                          #          2 = use/eq (see index 11)  
+    1026:[(0),(0),"Letttuce",       (0),   (0),    (2),    (1),     (10),    (0),    (7),    (0),   (0)],                          #          3 + 4 = Numerate
+    1027:[(0),(0),"Muddy Gloves",   (0),   (1),    (0),    (1),     (5),     (1),    (7),    (0),   (16)],                         
     1028:[(0),(0),"Dungeon Key",    (0),   (0),    (0),    (200),   (1),     (0),    (9),    (0),   (7)]}                          
     #Items: 0 Enum Merch, 1 Enum Player, 2 ItemName, 3 ATK, 4 DEF, 5 HEAL, 6  Value, 7 QntMAX, 8 QntPlayer, 9 ID, 10 ID_ON, 11 use/eq
 
     while True:  # >>>>>>>>>> MAIN GAME LOOP <<<<<<<<<<<
-        playerStats, playerStatPoints = Stats.LevelUp(playerStats, playerStatPoints, playerName)
+        playerStats, playerStatPoints, itemsDict = Stats.LevelUp(playerStats, playerStatPoints, playerName, itemsDict)
         userInput = input("\nWhat to do now?\n(1) Move\t(2) Inventory\t(3) Stats\t(4) Exit to main menu\n")
         match userInput:
             case "1": startLocation, location, playerStats, playerStatPoints, playerInventoryMoney, itemsDict = Move(
@@ -180,7 +180,7 @@ def Move(startLocation, location, playerStats, playerStatPoints, playerInventory
     location, playerStats, playerStatPoints, playerInventoryMoney, itemsDict = Encounter.Encounter(
     startLocation, location, playerStats, playerStatPoints, playerInventoryMoney, playerName, itemsDict)
 
-    playerStats, playerStatPoints = Stats.LevelUp(playerStats, playerStatPoints, playerName)
+    playerStats, playerStatPoints, itemsDict = Stats.LevelUp(playerStats, playerStatPoints, playerName, itemsDict)
 
     return startLocation, location, playerStats, playerStatPoints, playerInventoryMoney, itemsDict
 
