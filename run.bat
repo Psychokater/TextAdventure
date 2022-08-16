f8709c595f51570f971662d9a0ee158922e3c10f
Psychokater <psychokater@protonmail.com>
2022-08-16 15:49:13 +0200

    fixed bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 mode con: cols=140 lines=40
-"C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe" "%%~dp?\TextAdventure\Main.py"
+"C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe" "%~dp0\TextAdventure\Main.py"
 pause
