6a77df7fbf916a7b91101dcf67937c40bad68608
Psychokater <psychokater@protonmail.com>
2022-08-16 15:37:45 +0200

    getting "run" in another folder

--- TextAdventure/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mode con: cols=140 lines=40
-"C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe" "Main.py"
-pause
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+mode con: cols=140 lines=40
+"C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe" "Textadventure\Main.py"
+pause
