7bdf1faf154e4ee68b9689686d90e6eed914c713
Psychokater <psychokater@protonmail.com>
2022-08-23 10:09:10 +0200

    design fix

--- TextAdventure/Encounter.py
+++ TextAdventure/Encounter.py
@@ -80,7 +80,7 @@ def Encounter(startLocation, location, playerStats, playerStatPoints, playerInve
             
             (selectedDict[enemyID][6]()) 
             UserInputChoose = input(""\
-            f"\n\u26E8 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{round(playerStats[1],2)}{cl.RESET}\n"\
+            f"\n\u26E8 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{round(playerStats[1],2)}{cl.RESET}\n"\
             f"---------------------- VS ----------------------\n"\
             f"{strength} {cl.RED}{'{:<15}'.format(selectedDict[enemyID][0])}{cl.RESET}\t\tLVL {cl.BLUE}{selectedDict[enemyID][5]}{cl.RESET}\tHP {cl.BLUE}{round(selectedDict[enemyID][2],2)}/{round(enemyMaxHP,2)}{cl.RESET}\n {rank}\n\n"\
             f"What do you want to do now?\n(1) Fight\t(2) Inventory\t(3) Stats\t(0) Flee\n")
@@ -326,14 +326,14 @@ def Fight(startLocation, playerStats, playerStatPoints, selectedDict, enemyID, p
         
         if location == "dungeon castle" or location == "dungeon slumps" or location == "dungeon cave":
             UserInputFight = input(""\
-            f"\n\u2694 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{round(playerStats[1],2)}{cl.RESET}\n"\
+            f"\n\u2694 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{round(playerStats[1],2)}{cl.RESET}\n"\
             f"---------------------- VS ----------------------\n"\
             f"{strength} {cl.RED}{'{:<15}'.format(selectedDict[enemyID][0])}{cl.RESET}\t\tLVL {cl.BLUE}{selectedDict[enemyID][5]}{cl.RESET}\tHP {cl.BLUE}{round(selectedDict[enemyID][2],2)}/{round(enemyMaxHP,2)}{cl.RESET}\n {rank}\n\n"\
             f"(1) Attack\t(2) Inventory\t(3) Stats\t (0) Leave Dungeon\n")                                             # Fight (P = Player, E = Enemy)
             os.system('cls')
         else:
             UserInputFight = input(""\
-            f"\n\u2694 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{playerStats[1]}{cl.RESET}\n"\
+            f"\n\u2694 {cl.BLUE}{'{:<15}'.format(playerName)}{cl.RESET}\t\tLVL {cl.BLUE}{playerStats[0]}{cl.RESET}\tHP {cl.GREEN}{round(playerStats[2],2)}/{playerStats[1]}{cl.RESET}\n"\
             f"---------------------- VS ----------------------\n"\
             f"{strength} {cl.RED}{'{:<15}'.format(selectedDict[enemyID][0])}{cl.RESET}\t\tLVL {cl.BLUE}{selectedDict[enemyID][5]}{cl.RESET}\tHP {cl.BLUE}{round(selectedDict[enemyID][2],2)}/{round(enemyMaxHP,2)}{cl.RESET}\n {rank}\n\n"\
             f"(1) Attack\t(2) Inventory\t(3) Stats\t (0) Flee\n")                                             # Fight (P = Player, E = Enemy)
